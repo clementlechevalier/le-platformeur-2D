3d4a65145fa6385164aff0ebcd0034c880d33aa0
clementlechevalier <clement.chevalier03@free.fr>
2022-03-08 15:10:24 +0100

    ajout de ce qu'il faut pour crée hitBox et decor

---
+++ ecriture_hit_box.h
@@ -0,0 +1,17 @@
+#ifndef HITBOXX
+#define HITBOXX
+
+void creeHitBox();
+/**quand cette fonction est appelée,
+il suffit de regarder la ligne de commande et
+vous serez guidé afin de créer une hit box qui sera écrite
+dans un fichier nommé "hitBox.txt" que vous aurez dû créer au paravant.**/
+void creeRectangle();
+/**quand cette fonction est appelée
+regarde la ligne de commande
+il sera écrit dans un fichier text nommé decor.txt
+les propriété du réctangle crée.
+**/
+
+
+#endif
