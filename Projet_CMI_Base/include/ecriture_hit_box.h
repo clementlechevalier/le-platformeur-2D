3e8a688e0d045df1f188cdde61a58524180694d0
clementlechevalier <clement.chevalier03@free.fr>
2022-03-31 15:19:28 +0200

    fonction pour éditeur de map

--- Projet_CMI_Base/include/ecriture_hit_box.h
+++ Projet_CMI_Base/include/ecriture_hit_box.h
@@ -1,11 +1,14 @@
 #ifndef HITBOXX
 #define HITBOXX
 
-void creeHitBox();
+std::vector<int> positionMouse();
+void affichRect(std::vector<std::vector<int>> *);
+void modifHitBox(std::vector<std::vector<int>> *){
+
+void creeHitBox(std::vector<std::vector<int>> *);
 /**quand cette fonction est appelée,
 il suffit de regarder la ligne de commande et
-vous serez guidé afin de créer une hit box qui sera écrite
-dans un fichier nommé "hitBox.txt" que vous aurez dû créer au paravant.**/
+vous serez guidé afin de créer une hit box.**/
 void creeRectangle();
 /**quand cette fonction est appelée
 regarde la ligne de commande
--- Projet_CMI_Base/src/ecriture_hit_box.cpp
+++ Projet_CMI_Base/src/ecriture_hit_box.cpp
@@ -3,44 +3,79 @@
 #include <string>
 #include "ecriture_hit_box.h"
 
-//cree hit box avec clique souris pour position + mettre dans une liste de liste!! tous qui sera écrite dans un fichier constament
-void creeHitBox(){
-    int l,h,x,y,type;
-    std::string laString;
+std::vector<int> positionMouse(){
+    while(sf::Mouse::isButtonPressed(sf::Mouse::Left)==0){
+    }
+    std::cout<<sf::Mouse::getPosition().x<<" "<<sf::Mouse::getPosition().y<<"\n";
+        return std::vector<int>(sf::Mouse::getPosition().x,sf::Mouse::getPosition().y);
+}
+
+
+void creeHitBox(std::vector<std::vector<int>> *listeHitBox){
+    int type;
+    std::vector<int> supGauche,infDroit,laHitBox;
     std::cout<<"saisi le type, 1=plateforme, 2=porte, 3=piége:   ";std::cin>>type;
-    std::cout<<"saisi la largeur:   ";std::cin>>l;
-    std::cout<<"saisi la hauteur:   ";std::cin>>h;
-    std::cout<<"saisi l'abcsysse':   ";std::cin>>x;
-    std::cout<<"saisi l'ordonée':   ";std::cin>>y;
+    std::cout<<"clique sur l'angle supérieur gauche\n";supGauche=positionMouse();
+    std::cout<<"clique sur l'angle inférieur droit\n";infDroit=positionMouse();
     switch (type){
         case 1:
-            laString=std::to_string(l)+" "+std::to_string(h)+" "+std::to_string(x)+" "+std::to_string(y)+" "+std::to_string(type)+"\n";
+            {
+            laHitBox={infDroit[0]-supGauche[0],infDroit[1]-supGauche[1],supGauche[0],supGauche[1],type};
             break;
+            }
         case 2:
-            int i,o,xx,yy;
+            {
+            int i,o;
+            std::vector<int> reap;
             std::cout<<"saisi l'indice de la carte suivante':   ";std::cin>>i;
             std::cout<<"saisi l'orientation de la carte suivante':   ";std::cin>>o;
-            std::cout<<"saisi l'abscysse d'aparition':   ";std::cin>>xx;
-            std::cout<<"saisi l'ordonée d'aparition':   ";std::cin>>yy;
-            laString=std::to_string(l)+" "+std::to_string(h)+" "+std::to_string(x)+" "+std::to_string(y)+" "+std::to_string(type)+" "+std::to_string(i)+" "+std::to_string(o)+" "+std::to_string(xx)+" "+std::to_string(yy)+"\n";
+            std::cout<<"clique sur le point de réaparition";reap=positionMouse();
+            laHitBox={infDroit[0]-supGauche[0],infDroit[1]-supGauche[1],supGauche[0],supGauche[1],type,i,o,reap[0],reap[1]};
             break;
+            }
         case 3:
-            int xxx,yyy;
-            std::cout<<"saisi l'abscysse d'aparition':   ";std::cin>>xxx;
-            std::cout<<"saisi l'ordonée d'aparition':   ";std::cin>>yyy;
-            laString=std::to_string(l)+" "+std::to_string(h)+" "+std::to_string(x)+" "+std::to_string(y)+" "+std::to_string(type)+" "+std::to_string(xxx)+" "+std::to_string(yyy)+"\n";
+            {
+            std::vector<int> app;
+            std::cout<<"clique sur le point de réaparition";app=positionMouse();            
+            laHitBox={infDroit[0]-supGauche[0],infDroit[1]-supGauche[1],supGauche[0],supGauche[1],type,app[0],app[1]};
             break;
+            }
     }
-    std::ofstream fichierText;
-    fichierText.open("hitBox.txt",std::ofstream::app);
-    fichierText<<laString;
+    (*listeHitBox).push_back(laHitBox);
 }
 //afficher prend un rectangle dans la liste dont on prend l'indice dans le terminal et remplace le 5éme élément du vecteur de la liste par son type + 100
 //mettre une boucle while pour laisser le truc type 100 tant que une touche n'a pas été cliqué puis remettre le bon type
+void affichRect(std::vector<std::vector<int>> *listeHitBox){
+    int i;
+    std::cout<<"entre l'indice du rectangle a afficher en blanc:   ";std::cin>>i;
+    (*listeHitBox)[i][4]=(*listeHitBox)[i][4]+100;
+    std::cout<<"fait un clique gauche pour que tout revienne à la normale\n";
+    while(sf::Mouse::isButtonPressed(sf::Mouse::Left)==0){
+    }
+    (*listeHitBox)[i][4]=(*listeHitBox)[i][4]-100;
+}
+
 
 //fonction suppr qui demande l'indice du truc à supprimé(fonction au dessus sert à trouver l'indice)
+void supppr(std::vector<std::vector<int>> *listeHitBox){
+    int i;
+    std::cout<<"entre l'indice du rectangle a supprimer:   ";std::cin>>i;
+    (*listeHitBox).erase((*listeHitBox).begin()+i-1);
+}
 
 //modif carré qui avec indice donne les x y l h du carré et te permet de les modifier.
+void modifHitBox(std::vector<std::vector<int>> *listeHitBox){
+    int i,m;
+    std::cout<<"entre l'indice du rectangle a modifier:   ";std::cin>>i;
+    std::cout<<"son abcysse est: "<<(*listeHitBox)[i][2]<<"\nrentre ce que tu veut ajouter à l'abcysse:   ";std::cin>>m;
+    (*listeHitBox)[i][2]=(*listeHitBox)[i][2]+m;
+    std::cout<<"son ordonnée est: "<<(*listeHitBox)[i][3]<<"\nrentre ce que tu veut ajouter à l'ordonnée:   ";std::cin>>m;
+    (*listeHitBox)[i][3]=(*listeHitBox)[i][3]+m;
+    std::cout<<"sa largeur est: "<<(*listeHitBox)[i][0]<<"\nrentre ce que tu veut ajouter à la largeur:   ";std::cin>>m;
+    (*listeHitBox)[i][0]=(*listeHitBox)[i][0]+m;
+    std::cout<<"sa hauteur est: "<<(*listeHitBox)[i][1]<<"\nrentre ce que tu veut ajouter à la hauteur:   ";std::cin>>m;
+    (*listeHitBox)[i][1]=(*listeHitBox)[i][1]+m;
+}
 
 void creeRectangle(){
     float a,b;
