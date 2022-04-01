7a2c21cf16ab5f612ce23390591a0e4a891e79a1
clementlechevalier <clement.chevalier03@free.fr>
2022-04-01 12:44:03 +0200

    enlévemment de creerectangle

--- Projet_CMI_Base/include/ecriture_hit_box.h
+++ Projet_CMI_Base/include/ecriture_hit_box.h
@@ -9,12 +9,6 @@ void creeHitBox(std::vector<std::vector<int>> *);
 /**quand cette fonction est appelée,
 il suffit de regarder la ligne de commande et
 vous serez guidé afin de créer une hit box.**/
-void creeRectangle();
-/**quand cette fonction est appelée
-regarde la ligne de commande
-il sera écrit dans un fichier text nommé decor.txt
-les propriété du réctangle crée.
-**/
 
 
 #endif
--- Projet_CMI_Base/src/ecriture_hit_box.cpp
+++ Projet_CMI_Base/src/ecriture_hit_box.cpp
@@ -77,23 +77,3 @@ void modifHitBox(std::vector<std::vector<int>> *listeHitBox){
     (*listeHitBox)[i][1]=(*listeHitBox)[i][1]+m;
 }
 
-void creeRectangle(){
-    float a,b;
-    int x,y,boll,r,g,bl,indiceCarte;
-    std::ofstream fichierText;
-    std::string laString;
-    std::cout<<"pour que le perso passe au dessus du carré alors entre 1 sinon 0:   ";std::cin>>boll;
-    std::cout<<"largeur:   ";std::cin>>a;
-    std::cout<<"hauteur:   ";std::cin>>b;
-    std::cout<<"abscysse:   ";std::cin>>x;
-    std::cout<<"ordonnée:   ";std::cin>>y;
-    std::cout<<"rouge pour la couleur en rgb(entre 0 et 255):   ";std::cin>>r;
-    std::cout<<"vert pour la couleur en rgb(entre 0 et 255):   ";std::cin>>g;
-    std::cout<<"bleu pour la couleur en rgb(entre 0 et 255):   ";std::cin>>bl;
-    std::cout<<"donne l'indice de la carte ou on met le carré:   ";std::cin>>indiceCarte;
-    laString=std::to_string(boll)+" "+std::to_string(a)+" "+std::to_string(b)+" "+std::to_string(x)+" "+std::to_string(y)+" "+std::to_string(r)+" "+std::to_string(g)+" "+std::to_string(bl)+" "+std::to_string(indiceCarte)+"\n";
-    fichierText.open("decor.txt",std::ofstream::app);
-    fichierText<<laString;
-}
-
-
